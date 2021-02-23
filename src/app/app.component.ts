02268dde237282e9de4c8f25d4e8c314972f892e
r0m1 <romain.bertholon@gmail.com>
2021-02-23 14:11:03 +0100

    Add firebase to app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import firebase from 'firebase/app';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'bookshelves';
+
+  constructor() {
+    var firebaseConfig = {
+    
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+  }
+
 }
