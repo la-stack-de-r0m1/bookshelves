2a4cf14e2db57448ef059a087c1c22623d52e7c5
r0m1 <romain.bertholon@gmail.com>
2021-02-23 13:54:08 +0100

    Initial commit: - Create components - Create services - Create routes - Create navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,50 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SignupComponent } from './auth/signup/signup.component';
+import { SigninComponent } from './auth/signin/signin.component';
+import { BookListComponent } from './book-list/book-list.component';
+import { SingleBookComponent } from './book-list/single-book/single-book.component';
+import { BookFormComponent } from './book-list/book-form/book-form.component';
+import { HeaderComponent } from './header/header.component';
+import { BooksService } from './services/books.service';
+import { AuthGuardService } from './services/auth-guard.service';
+import { AuthService } from './services/auth.service';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
+
+const AppRoutes: Routes = [
+  {path: 'auth/signup', component: SignupComponent },
+  {path: 'auth/signin', component: SigninComponent },
+  {path: 'books', component: BookListComponent },
+  {path: 'books/new', component: BookFormComponent },
+  {path: 'books/view/:id', component: SingleBookComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignupComponent,
+    SigninComponent,
+    BookListComponent,
+    SingleBookComponent,
+    BookFormComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    RouterModule.forRoot(AppRoutes)
   ],
-  providers: [],
+  providers: [
+    AuthService,
+    BooksService,
+    AuthGuardService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
